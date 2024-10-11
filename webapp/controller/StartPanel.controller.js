df790ac602478eb059b1dec6d13afb0491b56325
jaroslawwielgus <jaroslaw.wielgus7557@gmail.com>
2024-10-11 14:14:52 +0200

    cutomers data from OData service in table

--- webapp/controller/StartPanel.controller.js
+++ webapp/controller/StartPanel.controller.js
@@ -1,20 +1,32 @@
+var ControllerInstance;
+
 sap.ui.define(
 	["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
 	(Controller, MessageToast) => {
 		"use strict";
 
 		return Controller.extend("ui5.walkthrough.controller.StartPanel", {
+			constructor: function () {
+				if (!ControllerInstance) {
+					ControllerInstance = this;
+					this.onInit();
+				}
+				return ControllerInstance;
+			},
+
+			onInit() {
+				var oDataModel = new sap.ui.model.odata.v2.ODataModel(
+					"https://thingproxy.freeboard.io/fetch/https://services.odata.org/V2/Northwind/Northwind.svc"
+				);
+				this.getView().setModel(oDataModel);
+			},
+
 			onShowClients() {
-				console.log("coś");
 				const oBundle = this.getView().getModel("i18n").getResourceBundle();
-				const sRecipient = this.getView()
-					.getModel()
-					.getProperty("/recipient/name");
+				const oModel = this.getOwnerComponent().getModel();
+				const sRecipient = oModel.getProperty("/recipient/name");
 				const sMsg = oBundle.getText("dataMsg", [sRecipient]);
-
 				MessageToast.show(sMsg);
-
-				console.log("cokolwiek");
 			},
 		});
 	}
